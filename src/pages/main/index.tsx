02a36c9842c9846fef713d5f3d9187b84cfcd1c8
박주호 <parkjooho0919@gmail.com>
2026-03-24 13:51:20 +0900

    feat: header 구현 및 css 기본 pretandard 작성

--- src/pages/main/index.tsx
+++ src/pages/main/index.tsx
@@ -1,5 +1,11 @@
+import Header from '@/components/header';
+
 const mainPage = () => {
-  return <div>main</div>;
+  return (
+    <div>
+      <Header />
+    </div>
+  );
 };
 
 export default mainPage;
